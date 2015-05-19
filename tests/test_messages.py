8983037476f19da8a4bd71ff2d5430d628caba32
Dave Hunt <dave.hunt@gmail.com>
2015-05-19 08:41:16 +0100

    Remove superfluous test classes

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -8,22 +8,21 @@ from unittestzero import Assert
 from pages.messages import MessagesPage
 
 
-class TestMessages:
+def test_create_message(mozwebqa, variables):
+    """Create a message"""
+    messages_page = MessagesPage(mozwebqa)
+    messages_page.open()
+    messages_page.login(variables['username'], variables['password'])
+    messages_page.create_message('<Hello>', '<strong>HTML</strong> allowed here')
+    Assert.equal(messages_page.notification, 'New entry was successfully posted')
+    Assert.equal(len(messages_page.messages), 1)
+    Assert.equal(messages_page.messages[0].title, '<Hello>')
+    Assert.equal(messages_page.messages[0].text, 'HTML allowed here')
 
-    def test_create_message(self, mozwebqa, variables):
-        """Create a message"""
-        messages_page = MessagesPage(mozwebqa)
-        messages_page.open()
-        messages_page.login(variables['username'], variables['password'])
-        messages_page.create_message('<Hello>', '<strong>HTML</strong> allowed here')
-        Assert.equal(messages_page.notification, 'New entry was successfully posted')
-        Assert.equal(len(messages_page.messages), 1)
-        Assert.equal(messages_page.messages[0].title, '<Hello>')
-        Assert.equal(messages_page.messages[0].text, 'HTML allowed here')
 
-    @pytest.mark.nondestructive
-    def test_empty(self, mozwebqa):
-        """Start with an empty list of messages"""
-        messages_page = MessagesPage(mozwebqa)
-        messages_page.open()
-        Assert.equal(messages_page.messages, [])
+@pytest.mark.nondestructive
+def test_empty(mozwebqa):
+    """Start with an empty list of messages"""
+    messages_page = MessagesPage(mozwebqa)
+    messages_page.open()
+    Assert.equal(messages_page.messages, [])
